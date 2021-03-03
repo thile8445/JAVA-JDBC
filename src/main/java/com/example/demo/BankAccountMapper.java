18f92315c9abb1a6b571e2a27e603ce0ae1209d2
thile8445 <thile8445@gmail.com>
2021-03-03 13:48:02 +0700

    jdbc

---
+++ src/main/java/com/example/demo/BankAccountMapper.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class BankAccountMapper implements RowMapper<BankAccountInfo> {
+	 
+    public static final String BASE_SQL //
+            = "Select ba.Id, ba.Full_Name, ba.Balance From Bank_Account ba ";
+ 
+    @Override
+    public BankAccountInfo mapRow(ResultSet rs, int rowNum) throws SQLException {
+ 
+        Long id = rs.getLong("Id");
+        String fullName = rs.getString("Full_Name");
+        double balance = rs.getDouble("Balance");
+ 
+        return new BankAccountInfo(id, fullName, balance);
+    }
+ 
+}
